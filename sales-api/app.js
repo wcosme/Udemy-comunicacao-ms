334780bd39d1471c05073c66ad93e72af33db5a7
Wallace Gonçalves <wallacecosme@gmail.com>
2022-04-16 20:03:54 -0300

    Sales-API created

---
+++ sales-api/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+const env = process.env;
+const PORT = env.PORT || 8082;
+
+app.get('/api/status', (req, res) =>{
+    return res.status(200).json({
+        service: "Sales-API",
+        status: "up",
+        httpstatus: 200,
+    });
+});
+
+
+app.listen(PORT, () =>{
+    console.info('Server started sucessfully at port ${PORT}');
+});
